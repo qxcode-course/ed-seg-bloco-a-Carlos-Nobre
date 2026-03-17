9c07007f2a56f7a5c4bf08f0537f7edccef97a11
Carlos-Nobre <carlosrodrigues2304n@gmail.com>
2026-03-16 23:53:57 -0300

    pedra na lua realizado

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,10 +1,64 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "math"
+    
+)
+
 func main() {
     var qntCompetidores int;
 
     fmt.Scan(&qntCompetidores)
 
-    var 
+    fist := make([]int, qntCompetidores)
+    second := make([]int, qntCompetidores)
+   
+
+    for i := 0; i < qntCompetidores; i++{
+        var primeiro,segundo int
+        fmt.Scan(&primeiro , &segundo)
+
+
+        
+        fist[i] = primeiro
+        second[i] = segundo
+
+    }
+
+    
+    
+    var campeao int 
+    var maiorDiff int 
+
+    for i := 0; i < qntCompetidores; i++{
+        if(fist[i] < 10 || second[i] < 10){
+            continue
+        }else{
+            campeao = i
+            maiorDiff = int(math.Abs(float64(fist[i] - second[i])))
+            break
+        }
+    }
+
+    
+
+    for i := 1; i < qntCompetidores; i++{ 
+        if(fist[i] < 10 || second[i] < 10){
+            continue
+        }
+
+        diff := int(math.Abs(float64(fist[i] - second[i])))
+
+        if maiorDiff > diff{
+            maiorDiff = diff
+            campeao = i
+        }
+    }
+    if campeao == 0{
+        fmt.Println("sem ganhador")
+    }else{
+        fmt.Println(campeao)
+    }
+
 
 }
