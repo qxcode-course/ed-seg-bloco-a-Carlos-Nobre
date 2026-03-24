7725b1308e854704b7352be8379ec1bc39fdd80c
Carlos-Nobre <carlosrodrigues2304n@gmail.com>
2026-03-24 00:02:34 -0300

    gomos feito

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,42 @@
 package main
 import "fmt"
-func main() {
-    fmt.Println("Hello, World!")
+
+type Gomo struct{
+    x,y int
 }
+
+func main() {
+    qtdGomos := 0
+    var dir rune
+
+    fmt.Scan(&qtdGomos)
+    fmt.Scanf("%c", &dir)
+
+    cobra := make([]Gomo, qtdGomos)
+
+    for i:= range cobra{
+        fmt.Scan(&cobra[i].x, &cobra[i].y)
+    }
+
+    for i := qtdGomos - 1; i > 0; i--{
+        cobra[i].x = cobra[i-1].x
+        cobra[i].y = cobra[i-1].y
+    }
+
+    switch dir{
+        case 'R':
+            cobra[0].x++
+        case 'L':
+            cobra[0].x--
+        case 'U':
+            cobra[0].y--
+        case 'D':
+            cobra[0].y++
+    }
+
+    for _, gomo := range cobra{
+        fmt.Println(gomo.x, gomo.y)
+    }
+
+}
+
