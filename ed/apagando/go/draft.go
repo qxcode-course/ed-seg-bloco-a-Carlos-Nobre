7333a5403ee4a8580aa736d595a52dae8ced9062
Carlos-Nobre <carlosrodrigues2304n@gmail.com>
2026-03-31 16:30:55 -0300

    apagando realizada

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,4 +1,36 @@
 package main
 import "fmt"
+
 func main() {
+	var numeroPessoas,numeroSaidas int
+	fmt.Scan(&numeroPessoas)
+
+	fila := make([]int, numeroPessoas)
+
+	for i := range fila{
+		fmt.Scan(&fila[i])
+	}
+
+	fmt.Scan(&numeroSaidas)
+
+	pessoasSairam := make(map[int]bool, numeroSaidas)
+
+	for i := 0; i < numeroSaidas; i++ {
+		var pessoa int
+		fmt.Scan(&pessoa)
+		pessoasSairam[pessoa] = true
+	}
+
+
+	for _, valor := range fila {
+		if pessoasSairam[valor]{
+			continue
+		}else{
+			fmt.Print(valor)
+			fmt.Print(" ")
+		}
+	}
+
+	fmt.Println("")
+
 }
