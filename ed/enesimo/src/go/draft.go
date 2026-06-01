79c8af0f2d922745987cd09f1ea124ed24867db0
Carlos-Nobre <carlosrodrigues2304n@gmail.com>
2026-06-01 16:06:30 -0300

    feat: Enesimo implementado

--- ed/enesimo/src/go/draft.go
+++ ed/enesimo/src/go/draft.go
@@ -1,5 +1,38 @@
 package main
+
 import "fmt"
+
+func IsPrimo(x int, div int) bool {
+	_, _ = x, div
+	if x < 2 {
+		return false
+	}
+	if div == x {
+		return true
+	}
+	if x%div == 0 {
+		return false
+	}
+
+	return IsPrimo(x, div+1)
+
+}
+func Nesimo(value int, test int, count int) int {
+
+	if count == value {
+		return test - 1
+	}
+	if IsPrimo(test, 2) {
+		count++
+	}
+
+	return Nesimo(value, test+1, count)
+}
 func main() {
-    fmt.Println("Hello, World!")
+	var x int
+	fmt.Scan(&x)
+
+	result := Nesimo(x, 1, 0)
+
+	fmt.Println(result)
 }
